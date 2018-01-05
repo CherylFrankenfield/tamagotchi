de4830792ffda442dfd308838f64316f82cd64ed
John Sawiris and Sam Kissee <sam@>
2018-01-04 16:27:28 -0800

    update specs

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,7 +1,7 @@
 import { Haiku } from './../src/js/haiku.js';
 
 describe('Haiku', function() {
-  it('it checks for vowels in silent', function() {
+  it('it checks for vowels in a word', function() {
     let haiku = new Haiku();
 
     expect(haiku.checkVowels("silent")).toEqual(2);
