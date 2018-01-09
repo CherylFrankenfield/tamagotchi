ec19e81a7680c125ba756957420deae99afff381
Cheryl Frankenfield and Sam Kissee <sam@>
2018-01-09 10:19:00 -0800

    Add env file and twitter require info

---
+++ src/js/twitter-interface.js
@@ -0,0 +1,15 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
