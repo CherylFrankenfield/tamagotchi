2065164144633215c67b2739780d23be34dff6fc
Cheryl Frankenfield and Sam Kissee <cheryl@>
2018-01-09 16:19:15 -0800

    api stuff

---
+++ src/js/promise.js
@@ -0,0 +1,16 @@
+// let promise = new Promise((resolve, reject) => {
+//   let value = true;
+//
+//   if(value) {
+//     resolve("This value is true!");
+//   }
+//   else {
+//     reject("This value is not true.");
+//   }
+//
+// })
+//
+// promise.then(
+//   resolved => console.log(resolve),
+//   error => console.log(error)
+// );
--- src/js/twitter-interface.js
+++ src/js/twitter-interface.js
@@ -1,31 +1,42 @@
-var Twitter = require('twitter');
-var tokens = require('twitter-tokens');
-var twitterMentions = require('twitter-mentions');
-var env = require('dotenv').config();
-
-
-var apiKey = require('./../.env').apiKey;
-var apiKeySec = require('./../.env').apiKey2;
-var apiAccess = require('./../.env').Access;
-var apiAccessSecret = require('./../.env').AccessSecret;
-
-
-var client = new Twitter({
-  consumer_key: process.env.apiKey,
-  consumer_secret: process.env.apiKeySec,
-  access_token: process.env.apiAccess,
-  access_token_secret: process.env.apiAccessSecret
-});
-
-client.get('favorites/list', function(error, tweets, response) {
-  if(error) throw error;
-  console.log(tweets);  // The favorites.
-  console.log(response);  // Raw response object.
-});
-
-// twitterMentions(client, '424119506508980224').then(mentions => {
-//   console.log(mentions); // [{…}, {…}, …]
+// var Twitter = require('twitter');
+// var tokens = require('twitter-tokens');
+// var twitterMentions = require('twitter-mentions');
+// var env = require('dotenv').config();
+//
+//
+// var apiKey = require('./../.env').apiKey;
+// var apiKeySec = require('./../.env').apiKey2secret;
+// var apiAccess = require('./../.env').Access;
+// var apiAccessSecret = require('./../.env').AccessSecret;
+//
+//
+// let oauth = {
+//   oauth_consumer_key: apiKey,
+//   oauth_consumer_secret: apiKeySec,
+//   oauth_access_token: apiAccess,
+//   oauth_access_token_secret: apiAccessSecret
+// };
+// const BASE_URL = 'https://api.twitter.com/oauth/authorize';
+//
+// // The identifier of the tweet
+// // The identifier of the tweet
+// let tweetId = `782399817351823360`;
+// let Options = {
+//   method: 'POST',
+//   uri: `${BASE_URL}/statuses/retweet/${tweetId}.json`,
+//   oauth
+// };
+//
+//
+// fetch(Options)
+//   .then(function (body) {
+//     // Successful response
+//     console.log(body);
+//   })
+//   .catch(function (err) {
+//     // If an error occurs
 // });
-
-
-// https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi?src=typd&q=%23bulbasaur
+//
+// // twitterMentions(oauth, '424119506508980224').then(mentions => {
+// //   console.log(mentions); // [{…}, {…}, …]
+// // });
